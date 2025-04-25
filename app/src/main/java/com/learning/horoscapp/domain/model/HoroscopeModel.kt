ff410127666b3a6f3eddcee410112746e6964a23
FernandoNLopez <lopezfer1998@outlook.com>
2025-04-25 20:49:19 -0300

    detail started

---
+++ app/src/main/java/com/learning/horoscapp/domain/model/HoroscopeModel.kt
@@ -0,0 +1,16 @@
+package com.learning.horoscapp.domain.model
+
+enum class HoroscopeModel {
+    Aries,
+    Taurus,
+    Gemini,
+    Cancer,
+    Leo,
+    Virgo,
+    Libra,
+    Scorpio,
+    Sagittarius,
+    Capricornio,
+    Aquarius,
+    Pisces
+}
--- app/src/main/java/com/learning/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/learning/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -11,9 +11,12 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.learning.horoscapp.databinding.FragmentHoroscopeBinding
+import com.learning.horoscapp.domain.model.HoroscopeInfo.*
+import com.learning.horoscapp.domain.model.HoroscopeModel
 import com.learning.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -46,7 +49,23 @@ class HoroscopeFragment : Fragment() {
 
     private fun initList() {
         horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
-                Toast.makeText(context, getString(it.name), Toast.LENGTH_LONG).show()
+            val type = when(it){
+                Aquarius -> HoroscopeModel.Aquarius
+                Aries -> HoroscopeModel.Aries
+                Cancer -> HoroscopeModel.Cancer
+                Capricorn -> HoroscopeModel.Capricornio
+                Gemini -> HoroscopeModel.Gemini
+                Leo -> HoroscopeModel.Leo
+                Libra -> HoroscopeModel.Libra
+                Pisces -> HoroscopeModel.Pisces
+                Sagittarius -> HoroscopeModel.Sagittarius
+                Scorpio -> HoroscopeModel.Scorpio
+                Taurus -> HoroscopeModel.Taurus
+                Virgo -> HoroscopeModel.Virgo
+            }
+            findNavController().navigate(
+                HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity(type)
+            )
         })
 
         binding.rvHoroscope.apply {
