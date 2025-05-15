baa4e64af95003b174c5b4bba27ee6ecec924a6e
FernandoNLopez <lopezfer1998@outlook.com>
2025-05-15 13:01:47 -0300

    build Types and Interceptors

--- app/src/main/java/com/learning/horoscapp/data/network/NetworkModule.kt
+++ app/src/main/java/com/learning/horoscapp/data/network/NetworkModule.kt
@@ -1,11 +1,15 @@
 package com.learning.horoscapp.data.network
 
+import com.learning.horoscapp.BuildConfig.BASE_URL
+import com.learning.horoscapp.data.core.interceptors.AuthInterceptor
 import com.learning.horoscapp.data.repository.RepositoryImplementation
 import com.learning.horoscapp.domain.Repository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,8 +20,11 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl("https//newastro.vercel.app/")
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -30,5 +37,18 @@ object NetworkModule {
     @Provides
     fun provideRepository(apiService: HoroscopeAPIService) : Repository {
         return RepositoryImplementation(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor):OkHttpClient{
+
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(authInterceptor)
+            .build()
     }
 }
